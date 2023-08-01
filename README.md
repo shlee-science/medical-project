3ec6de03a2c253c1d14967bd466c6ff07ba84441
sihyeong671 <bshlab671@naver.com>
2023-08-02 01:56:26 +0900

    fix: streamlit, add: ipynb and readme content

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 # TODO
-- [ ] test 셋에 레이블 받을 수 있는지 물어보기
-- [ ] 원본 데이터셋에서 type이 정확히 뭘 나타내는지 알아보기(심각도인지?)
-- [ ] 배경 제거 및 합성
+- [ ] 데이터 셋 배경 제거 및 합성
+- [ ] 타입별 예측(multi class)
+- [ ] vision + tabular (multi modal)
+- [ ] 레이블 없는 부분 확인 필요 및 tabular 데이터 사용가능한지 확인
+- [ ] 이미지 3 ~ 5 가량 회전되어 있는 이미지가 있어서 augmentation 적용 필요
+
+---
+
+**[관련 논문](https://www.nature.com/articles/s42003-019-0635)**
+> 논문에서는 Object Detection 관련 Task로 학습할 수 있게 되어있음
+> ### 간단한 과정
+> 부산대학병원에서 받은 labeling 데이터는 type만 나타내져 있으므로
+> 
+> 1. 학습된 Yolov8 프레임워크의 딥러닝 모델을 활용해 pose keypoint를 찾고 bbox를 탐색(_augmentation/sample_pipeline.ipynb_)
+> 2. efficientnet과 같은 cnn based model을 선정후 학습
+> 3. 저장된 모델을 통해 추론 - [테스트 영상](https://drive.google.com/file/d/1WDoIKtjJyNNEP80vvJB8RZYELtwyms_q/view?usp=sharing)
+> 
--- streamlit.py
+++ streamlit.py
@@ -1,7 +1,8 @@
 from glob import glob
-import numpy as np
+import time
 import cv2
-from PIL import Image
+import numpy as np
+from PIL import Image, ImageOps
 import torch
 import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
@@ -21,43 +22,58 @@ def get_model():
   
   return keypoint_model, model
 
-@st.cache_data
-def get_testimg_list():
-  # paths = sorted(glob("data/test/*"))
-  paths = glob("data\\test\\*")
-  img_lst = []
-  for path in paths:
-    if "X" not in path:
-      img_lst.append(path)
-  return sorted(img_lst,  key=lambda x: int(x.split("\\")[-1].split(".")[0]))
+# -----이미지 경로 통해 사진 불러오는 경우 -----
+
+# @st.cache_data
+# def get_testimg_list():
+#   # paths = sorted(glob("data/test/*"))
+#   paths = glob("data\\test\\*")
+#   img_lst = []
+#   for path in paths:
+#     if "X" not in path:
+#       img_lst.append(path)
+#   return sorted(img_lst,  key=lambda x: int(x.split("\\")[-1].split(".")[0]))
+
+# with st.sidebar:
+#   img_path = st.selectbox("경로를 선택하세요", get_testimg_list())
+
 
 st.title("척추측만증 예측")
 
-with st.sidebar:
-  img_path = st.selectbox("경로를 선택하세요", get_testimg_list())
 
-if img_path is None:
-  pass
-else:
-  img = cv2.imread(img_path)
-  img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-  st.image(img)
+img_byte = st.file_uploader("이미지를 선택하세요", type=["jpg", "png", "jpeg"])
+
+
+if img_byte is not None:
+  img = Image.open(img_byte).convert('RGB')
+  img = ImageOps.exif_transpose(img)
+  img = np.array(img)
+
+  st.image(
+    img,
+    caption=f"image shape: {img.shape[0:2]}",
+    use_column_width=True,
+  )
   k_model, model = get_model()
 
-test_aug = A.Compose([
-  A.Normalize(),
-  A.Resize(384, 384),
-  ToTensorV2()
-])
+  test_aug = A.Compose([
+    A.Normalize(),
+    A.Resize(384, 384),
+    ToTensorV2()
+  ])
 
-crop_img = crop_preprocess(img, k_model)
-test_img = test_aug(image=crop_img)["image"].unsqueeze(0)
+  start = time.time()
+  crop_img = crop_preprocess(img, k_model)
+  test_img = test_aug(image=crop_img)["image"].unsqueeze(0)
 
-model.eval()
-with torch.no_grad():
-  result = model(test_img)
-  result = result.squeeze(0).cpu().numpy()[0]
+  model.eval()
+  with torch.no_grad():
+    result = model(test_img)
+    result = result.squeeze(0).cpu().numpy()[0]
 
-st.text(f"척추 측만증일 확률: {result*100:.3f}%")
+  end = time.time()
+
+  st.text(f"추론 시간(cpu): {end-start:.3f}sec")
+  st.text(f"척추 측만증일 확률: {result*100:.3f}%")
 
 
